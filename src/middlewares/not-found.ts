ac2a7faeae3bb47f86a2ab9ed15c966fd4cf79f5
saif ali shaikh <saifalie14@gmail.com>
2024-10-23 16:36:25 +0530

    fix bugs

--- src/middlewares/not-found.ts
+++ src/middlewares/not-found.ts
@@ -1,6 +1,6 @@
-import { Request, Response } from "express";
-import { StatusCodes } from "http-status-codes";
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
-export const notFound = (req:Request,res:Response)=>{
-    res.status(StatusCodes.NOT_FOUND).send
-}
+export const notFoundMiddleware = (req: Request, res: Response) => {
+    res.status(StatusCodes.NOT_FOUND).send('Route does not exists');
+};
