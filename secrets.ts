1e696f2d0b529d300ef933638e90db0c92c41cfc
saif ali shaikh <saifalie14@gmail.com>
2024-10-23 16:36:51 +0530

    setup server

--- secrets.ts
+++ secrets.ts
@@ -6,3 +6,4 @@ dotenv.config({
 
 export const PORT = process.env.PORT;
 export const DB_URL = process.env.DB_URL;
+export const CORS = process.env.CORS;
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
 import express, { Request, Response, Express, NextFunction } from 'express';
-import { PORT } from '../secrets';
+import { CORS, PORT } from '../secrets';
 
 import { Server } from 'socket.io';
 import http from 'http';
 import { handleSocketConnection } from './sockets/socket';
 import { errorMiddleware } from './middlewares/errors';
 import { connectDB } from './config/connect';
+import { notFoundMiddleware } from './middlewares/not-found';
+import { StatusCodes } from 'http-status-codes';
+import { ApiResponse } from './utils/ApiResponse';
 
 const app: Express = express();
 
 app.use(express.json());
 
+// server instance
 const server = http.createServer(app);
 
+// io instance
 const io = new Server(server, {
     cors: {
-        origin: '*'
+        origin: `${CORS}`
     }
 });
 
@@ -32,12 +37,14 @@ handleSocketConnection(io);
 //Routes
 
 app.get('/saif', (req, res) => {
-    res.send('darling how are you, feeling good !!');
+    res.status(StatusCodes.OK).json(new ApiResponse(StatusCodes.OK, { data: 'data' }, 'data is there'));
 });
 
 //Middlewares
 app.use(errorMiddleware);
+app.use(notFoundMiddleware);
 
+// initializing the DB and server
 const start = async () => {
     try {
         await connectDB()
