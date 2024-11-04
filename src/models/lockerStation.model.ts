0fdf216ba886cc8e889bb6b05c551d80fa1ecf2c
saif ali shaikh <saifalie14@gmail.com>
2024-11-04 23:55:18 +0530

    add opening hours and interface

--- src/models/lockerStation.model.ts
+++ src/models/lockerStation.model.ts
@@ -1,8 +1,38 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose, { Document, Schema, Types } from 'mongoose';
 
-const LockerStationSchema = new Schema(
+enum WeekDay {
+    MONDAY = 'Mon',
+    TUESDAY = 'Tue',
+    WEDNESDAY = 'Wed',
+    THURSDAY = 'Thu',
+    FRIDAY = 'Fri',
+    SATURDAY = 'Sat',
+    SUNDAY = 'Sun'
+}
+
+interface LockerStation extends Document {
+    station_name: string;
+    status: 'OPEN' | 'CLOSED' | 'MAINTENANCE';
+    location: {
+        latitude: number;
+        longitude: number;
+        address: string;
+    };
+    images: { url: string }[];
+    ratings: Types.ObjectId[];
+    reviews: Types.ObjectId[];
+    lockers: Types.ObjectId[];
+    opening_hours: {
+        day: WeekDay;
+        opens_at: string;
+        closes_at: string;
+        is_closed: boolean;
+    }[];
+}
+
+const LockerStationSchema = new Schema<LockerStation>(
     {
-        stationName: {
+        station_name: {
             type: String,
             required: true,
             unique: true
@@ -53,9 +83,30 @@ const LockerStationSchema = new Schema(
                 type: Schema.Types.ObjectId,
                 ref: 'Locker'
             }
+        ],
+        opening_hours: [
+            {
+                day: {
+                    type: String,
+                    enum: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
+                    default: 'Mon'
+                },
+                opens_at: {
+                    type: String,
+                    default: '06:00'
+                },
+                closes_at: {
+                    type: String,
+                    default: '22:00'
+                },
+                is_closed: {
+                    type: Boolean,
+                    default: false
+                }
+            }
         ]
     },
     { timestamps: true }
 );
 
-export const LockerStation = mongoose.model('LockerStation', LockerStationSchema);
+export const LockerStation = mongoose.model<LockerStation>('LockerStation', LockerStationSchema);
