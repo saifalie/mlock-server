761410a0bf6f8619e626ec7e04a42faab1d91e41
saif ali shaikh <saifalie14@gmail.com>
2024-11-04 17:20:51 +0530

    add some secrets

--- secrets.ts
+++ secrets.ts
@@ -7,3 +7,7 @@ dotenv.config({
 export const PORT = process.env.PORT;
 export const DB_URL = process.env.DB_URL;
 export const CORS = process.env.CORS;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
