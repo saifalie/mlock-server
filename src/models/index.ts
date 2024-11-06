923a60158cf8490d45501f6ea2fa9690127bef16
saif ali shaikh <saifalie14@gmail.com>
2024-11-06 21:51:02 +0530

    centeralize model imports

---
+++ src/models/index.ts
@@ -0,0 +1,6 @@
+import { Locker } from './locker.model.js';
+import { LockerStation } from './lockerStation.model.js';
+import { Rating, Review } from './rating&review.model.js';
+import { User } from './user.model.js';
+
+export { User, Locker, LockerStation, Rating, Review };
