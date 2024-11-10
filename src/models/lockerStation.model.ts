5ffdd56e276f77b84a1e8d6c4cffffcbc8cb1df4
saif ali shaikh <saifalie14@gmail.com>
2024-11-10 17:03:22 +0530

    Update lockerStation model

--- src/models/lockerStation.model.ts
+++ src/models/lockerStation.model.ts
@@ -44,8 +44,12 @@ const LockerStationSchema = new Schema<LockerStation>(
             default: 'CLOSED'
         },
         location: {
-            type: { type: String, required: true },
-            coordinates: []
+            type: { type: String, enum: ['Point'], required: true },
+            coordinates: {
+                type: [Number],
+                required: true,
+                index: '2dsphere'
+            }
         },
         address: { type: String, required: true },
 
