9f4d5ef3869f70f0640c2f1224e97abb5fd8156e
saif ali shaikh <saifalie14@gmail.com>
2024-12-12 19:08:10 +0530

    Update src/routes/index.routes.ts

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import lockerStationRoutes from './lockerStation.routes.js';
+import fileRoutes from './file.routes.js';
+import authRoutes from './auth.routes.js';
 
 const rootRouter: Router = Router();
 
 rootRouter.use('/lockerStation', lockerStationRoutes);
+rootRouter.use('/file', fileRoutes);
+rootRouter.use('/auth', authRoutes);
 
 export default rootRouter;
