dd32b22b23e8b8e25e9f691b91f1c784d295dcf3
saifalie <saifalie14@gmail.com>
2025-01-16 13:49:17 +0530

    testing

--- src/sockets/socket.ts
+++ src/sockets/socket.ts
@@ -3,6 +3,8 @@ import { Server } from 'socket.io';
 export const handleSocketConnection = (io: Server) => {
     io.on('connection', (socket) => {
         console.log('socketId: ', socket.id);
+        console.log('hey its saif here');
+        
     });
 
     io.on('disconnect', (socket) => {
